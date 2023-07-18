0b4a317371b5a008d892bc0fa2c226c48eeda761
saleh <saleh.abuzayed@digitalgates.net>
2023-07-18 13:14:39 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AzureCR.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
